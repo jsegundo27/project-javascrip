cda672d1ed6e510d10183106d1ab6664c338c1e2
edwinSegundo <edwin27segundo@gmail.com>
2022-05-12 11:24:29 -0500

    codigo mejorado en main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
-var btn_red=document.getElementById("btn-red");
-     var btn_azul=document.getElementById("btn-azul");
-     var btn_verde=document.getElementById("btn-verde");
-     var color=document.getElementById('color');
-     btn_red.addEventListener('click',function(e){
-    
-        document.body.className="red";
-        color.innerHTML="Red";
-        e.preventDefault();
-    
-     });
-     btn_azul.addEventListener('click',function(e){
-   
-          document.body.className="azul"
-          color.innerHTML="Blue";
-          e.preventDefault();
-    
-     });
-     btn_verde.addEventListener('click',function(e){
-        
-           document.body.className="verde";
-           color.innerHTML="Green";
-           e.preventDefault();
-    
-     });
+//variables
+
+var btn_red   = document.getElementById("btn-red");
+var btn_azul  = document.getElementById("btn-azul");
+var btn_verde = document.getElementById("btn-verde");
+var color     = document.getElementById('color');
+
+//funcion de inicio
+function init(){
+   event_click();
+}
+
+//evetos
+function event_click(){
+       btn_red.addEventListener('click',clickRed);
+       btn_azul.addEventListener('click',clickAzul);
+       btn_verde.addEventListener('click',clickVerde);
+}
+
+
+function colorear(colorEsp,colorEng){
+     document.body.className=colorEsp;  
+     color.innerHTML=colorEng;
+}
+
+//metodos click
+function clickVerde(e){
+     colorear('verde','Green');
+     e.preventDefault();
+}
+function clickRed(e){
+     colorear('red','Red');
+     e.preventDefault();
+}
+function clickAzul(e){
+     colorear('azul','Blue');  
+     e.preventDefault();
+}
+init();
+
+
